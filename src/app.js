309fa5246b68729b14d6e8101df0394ff00b1a5a
lloyderson <jlloyd47@my.bcit.ca>
2022-03-21 10:58:32 -0700

    finished part 3 of lab. Used a function in javascript to get the avg of students who have over 80%.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
 function logfunc() {
     console.log(document.querySelector("p"))
 }
+
+const students = [
+    { name: "Tasha", grades: [ 80, 67, 75, 62 ] },
+    { name: "Tuvok", grades: [ 90, 88, 91, 92 ] },
+    { name: "Dax", grades: [ 88, 90, 89, 85 ] },
+    { name: "Neelix", grades: [ 60, 54, 48, 50 ] }
+  ]
+
+function avggradestu() {
+    const studentsScholarship = []
+    for (let goodstu of students) {
+        let avg = 0
+        for (let grade of goodstu.grades) {
+            avg += grade
+    }
+  let average = avg/goodstu.grades.length
+  if (average > 80) {
+    studentsScholarship.push({name: goodstu.name, gradeAvg: average})
+        }
+    }
+    return studentsScholarship
+}
+avggradestu()
+console.log(avggradestu())
